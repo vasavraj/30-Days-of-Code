2f8c92f3a40133f5d640e376b5e1ae0d46d0655f
vasavraj <60337727+vasavraj@users.noreply.github.com>
2020-05-22 22:47:51 +0530

    Updated Solution.js

--- 4 - Class vs. Instance/Solution.js
+++ 4 - Class vs. Instance/Solution.js
@@ -1,23 +1,50 @@
-function Person(initialAge) {
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+ 
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+ 
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+ 
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+ 
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+//Given data above.
+ 
+function Person(initialAge){
     // Add some more code to run some checks on initialAge
-    if (initialAge < 0) {
-        console.log("Age is not valid, setting age to 0.");
-        this.age = 0;
+    if (initialAge &amp;lt; 0){ console.log("Age is not valid, setting age to 0."); age = 0; } else { age = initialAge; } this.amIOld=function(){ // Do some computations in here and print out the correct statement to the console if (age &amp;gt;= 0 &amp;amp;&amp;amp; age &amp;lt; 13) { console.log("You are young."); } else if (age &amp;gt;=13 &amp;amp;&amp;amp; age &amp;lt; 18) {
+        console.log("You are a teenager.");
     } else {
-        this.age = initialAge;
+        console.log("You are old.");
     }
-    this.amIOld = function () {
-        // Do some computations in here and print out the correct statement to the console
-        if (this.age < 13) {
-            console.log('You are young.');
-        } else if (this.age >= 13 && this.age < 18) {
-            console.log("You are a teenager.");
-        } else {
-            console.log('You are old.');
-        }
-    };
-    this.yearPasses = function () {
-        // Increment the age of the person in here
-        this.age += 1;
-    };
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+       age+=1;
+   };
+}
+ 
+function main() {
+ 
+var T=parseInt(readLine());
+for(i=0;i&amp;lt;T;i++){
+    var age=parseInt(readLine());
+    var p=new Person(age);
+    p.amIOld();
+    for(j=0;j&amp;lt;3;j++){
+        p.yearPasses();
+ 
+    }
+    p.amIOld();
+    console.log("");
+    }
 }
